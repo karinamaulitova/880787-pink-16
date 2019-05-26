097c24a384e650ebdc664d22f130d16c0dfb564f
Karina <karinamaulitova@gmail.com>
2019-05-27 01:04:07 +0300

    Починены ошибки кроссбраузерности

--- gulpfile.js
+++ gulpfile.js
@@ -97,5 +97,5 @@ gulp.task("html", function () {
     .pipe(gulp.dest("build"));
 })
 
-gulp.task("build", gulp.series("clean","copy","css", "sprite", "html"));
+gulp.task("build", gulp.series("clean","copy","css", "webp", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
