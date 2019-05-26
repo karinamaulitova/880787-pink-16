244e284f3986664c87a33cfde29a9e69b7e010df
Karina <karinamaulitova@gmail.com>
2019-05-26 04:12:29 +0300

    Добавлен refresh для server

--- gulpfile.js
+++ gulpfile.js
@@ -39,8 +39,8 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/img/{icon,logo}-*.svg", gulp.series("sprite","html"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css", "refresh"));
+  gulp.watch("source/img/{icon,logo}-*.svg", gulp.series("sprite","html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
 
